6e7b2a8477d9dc63e1668ebe726576cebb25f61d
juliorodrigues1 <juliorodrigues.ac@gmail.com>
2021-12-23 19:52:11 -0500

    fazendo busca binaria no array

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+    public function search(Request $request)
+    {
+        $pokemonIsFound = $this->binarySearch($request->query('nome'), $this->index());
+        if ($pokemonIsFound){
+            return response()->json(true , 200);
+        }else{
+            return response()->json(false, 404);
+        }
+    }
+
+    private function binarySearch($name, $pokemons){
+        $middle = intdiv(count($pokemons), 2);
+
+        if($name == $pokemons[$middle]['nome']) return true;
+        if(count($pokemons) == 1) return false;
+
+        if($name < $pokemons[$middle]['nome']) return $this->binarySearch($name, array_slice($pokemons, 0, $middle));
+        return $this->binarySearch($name, array_slice($pokemons, $middle));
+    }
+
+    private function index()
+    {
+        $pokemonsDB = Pokemon::orderBy('nome')->get();
+        $pokemon = [];
+        foreach ($pokemonsDB as $key => $pokemonDB){
+            $pokemon[$key]['nome'] = $pokemonDB->nome;
+        }
+
+        return $pokemon;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('pesquisar/pokemon', [\App\Http\Controllers\PokemonController::class, 'search']);
